814f616266e4e74205bf4ed4febecf7b8898211c
parkeunhyo <eun261126@naver.com>
2024-09-27 16:51:40 +0900

    t

--- jspProject/src/main/java/pjh/MemberMgr.java
+++ jspProject/src/main/java/pjh/MemberMgr.java
@@ -9,7 +9,7 @@ import java.util.Vector;
 import net.nurigo.sdk.message.model.Message;
 import net.nurigo.sdk.message.service.DefaultMessageService;
 import net.nurigo.sdk.NurigoApp;
-import net.nurigo.sdk.message.exception.NurigoMessageNotReceivedException;
+import net.nurigo.sdk.message.exception.NurigoMessageNotReceivedException; 
 
 public class MemberMgr {
 
